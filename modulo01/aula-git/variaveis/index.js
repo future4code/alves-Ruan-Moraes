2cb49831c5ba6683b232022b28ff923e25221c39
Ruan Marques <ruanmarques96@gmail.com>
2022-05-09 17:07:37 -0300

    ArquivoJS

---
+++ modulo01/aula-git/variaveis/index.js
@@ -0,0 +1,36 @@
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+
+console.log(a, b)
+
+// O resultado do primeiro console é 10 já o do segundo é 5 
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c) 
+
+// o resultado do console é 10 e 10 e 10
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+/* prompt = abre caixa de dialogo no site
+alert = exibe uma mensagem no navegador por meio de uma caixa de diálogo, que nada mais é que uma pequena janela popup.
+
+/* let HorasDia = prompt("Quantas horas você trabalha por dia?")
+let GanhosDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${HorasDia/GanhosDia} por hora`)
+*/
+
+
+
+
