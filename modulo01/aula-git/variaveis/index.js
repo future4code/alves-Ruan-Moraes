ba7a318e518b56892dc8a851d705c689b4c96a86
Ruan Marques <ruanmarques96@gmail.com>
2022-05-09 19:05:01 -0300

    ArquivosJs

--- modulo01/aula-git/variaveis/index.js
+++ modulo01/aula-git/variaveis/index.js
@@ -1,21 +1,21 @@
-let a = 10
-let b = 10
+let d = 10
+let f = 10
 
-console.log(b)
+console.log(f)
 
-b = 5
+f = 5
 
-console.log(a, b)
+console.log(d, f)
 
 // O resultado do primeiro console é 10 já o do segundo é 5 
 
-let a = 10
-let b = 20
-c = a
-b = c
-a = b
+let g = 10
+let l = 20
+u = g
+l = u
+g = l
 
-console.log(a, b, c) 
+console.log(g, l, u) 
 
 // o resultado do console é 10 e 10 e 10
 
@@ -30,6 +30,44 @@ alert = exibe uma mensagem no navegador por meio de uma caixa de diálogo, que n
 let GanhosDia = prompt("Quanto você recebe por dia?")
 alert(`Voce recebe ${HorasDia/GanhosDia} por hora`)
 */
+// Exercícios de escrita de código
+
+let nome = undefined 
+let idade = undefined
+
+console.log(typeof nome,typeof idade)
+
+// o resultado de um valor nao definido é = undefined 
+
+nome = prompt("qual é o seu nome?")
+idade = prompt("qual a sua idade?")
+console.log(nome ,idade)
+// a variavel atribuiu um valor
+
+console.log("Olá", nome ,"voçê tem", idade,"anos")
+
+let time = prompt("vc é flamenguista?")
+let roupa = ("vc está usando a roupa de cor azul?")
+let jogos = ("vc esta jogando free fire? ")
+console.log(time ,roupa ,jogos)
+//-------------------------------------------
+
+let a = 10
+let b = 25
+let c = undefined 
+ = b //25
+b = a // 25
+c 
+console.Log("O novo valor de a é", é a)
+console.Log("O novo valor de b é , é b")
+
+
+
+
+
+
+
+
 
 
 
