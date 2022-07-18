4a4c2aa5baee3d3f7610fcf99cab8157ea87bdbf
Ruan Marques <ruanmarques96@gmail.com>
2022-07-17 23:18:05 -0300

    bugs - nao terminado!

--- modulo3/projeto-labex/projeto-labex/src/App.js
+++ modulo3/projeto-labex/projeto-labex/src/App.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { HomePage } from './pages/HomePage'
-import {ListTrip} from './pages/ListTripsPage'
-import { ApplicationForm } from './pages/ApplicationFormPage'
-import { LoginPage } from './pages/LoginPage'
-
+import { AppConteiner } from "../Rotas/Route";
 
 function App() {
   return (
     <div>
-      <HomePage/>
-      <ListTrip/>
-      <ApplicationForm/>
-      <LoginPage/>
-     
+      
+     <AppConteiner/>
     </div>
   );
 }
